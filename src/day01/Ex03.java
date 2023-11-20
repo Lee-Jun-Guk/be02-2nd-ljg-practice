364c36ffdd99265f69e80abe313536810c33b266
Lee-Jun-Guk <ell0404@naver.com>
2023-11-20 15:05:56 +0900

    Practice

---
+++ src/day01/Ex03.java
@@ -0,0 +1,64 @@
+package day01;
+
+public class Ex03 {
+    public static void main(String[] args) {
+        // 1. 다음과 같이 정사각형을 출력하는 코드 작성
+        /*
+        ----------
+         *****
+         *****
+         *****
+         *****
+         *****
+        ----------
+         */
+        System.out.println("/*");
+        System.out.println("----------");
+        System.out.println("*****");
+        System.out.println("*****");
+        System.out.println("*****");
+        System.out.println("*****");
+        System.out.println("*****");
+        System.out.println("----------");
+        System.out.println("*/");
+
+        // 2. 다음과 같이 삼각형을 출력하는 코드 작성
+        /*
+        ----------
+         *
+         **
+         ***
+         ****
+         *****
+        ----------
+         */
+        System.out.println("/*");
+        System.out.println("----------");
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        System.out.println("*****");
+        System.out.println("----------");
+        System.out.println("/*");
+        // 3. 다음과 같이 삼각형을 출력하는 코드 작성
+        /*
+        ----------
+             *
+            ***
+           *****
+          ********
+         **********
+         ----------
+         */
+        System.out.println("/*");
+        System.out.println("----------");
+        System.out.println("    *");
+        System.out.println("   ***");
+        System.out.println("  *****");
+        System.out.println(" *******");
+        System.out.println("**********");
+        System.out.println("----------");
+        System.out.println("/*");
+    }
+}
